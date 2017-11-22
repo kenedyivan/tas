0a55fccfd221e88d4c7039e4a3bc0b9c7e92a760
kenedyivan <kenedyakenaivan@gmail.com>
2017-11-22 10:48:29 +0300

    modified panel_layout.blade

--- resources/views/layouts/panel_layout.blade.php
+++ resources/views/layouts/panel_layout.blade.php
@@ -128,7 +128,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle dropdown-at" data-toggle="dropdown"><span
-                                    class=" name-caret">Rackham<i class="caret"></i></span><img src="images/wo.jpg"></a>
+                                    class=" name-caret">Kenedy<i class="caret"></i></span><img src="images/wo.jpg"></a>
                         <ul class="dropdown-menu " role="menu">
                             <li><a href="profile.html"><i class="fa fa-user"></i>Edit Profile</a></li>
                             <li><a href="inbox.html"><i class="fa fa-envelope"></i>Inbox</a></li>
@@ -165,68 +165,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
                             </a>
                         </li>
 
-                        <li>
-                            <a href="#" class=" hvr-bounce-to-right"><i class="fa fa-indent nav_icon"></i> <span
-                                        class="nav-label">Menu Levels</span><span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li><a href="graphs.html" class=" hvr-bounce-to-right"> <i
-                                                class="fa fa-area-chart nav_icon"></i>Graphs</a></li>
 
-                                <li><a href="maps.html" class=" hvr-bounce-to-right"><i
-                                                class="fa fa-map-marker nav_icon"></i>Maps</a></li>
-
-                                <li><a href="typography.html" class=" hvr-bounce-to-right"><i
-                                                class="fa fa-file-text-o nav_icon"></i>Typography</a></li>
-
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="inbox.html" class=" hvr-bounce-to-right"><i class="fa fa-inbox nav_icon"></i> <span
-                                        class="nav-label">Inbox</span> </a>
-                        </li>
-
-                        <li>
-                            <a href="gallery.html" class=" hvr-bounce-to-right"><i class="fa fa-picture-o nav_icon"></i>
-                                <span class="nav-label">Gallery</span> </a>
-                        </li>
-                        <li>
-                            <a href="#" class=" hvr-bounce-to-right"><i class="fa fa-desktop nav_icon"></i> <span
-                                        class="nav-label">Pages</span><span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li><a href="404.html" class=" hvr-bounce-to-right"> <i
-                                                class="fa fa-info-circle nav_icon"></i>Error 404</a></li>
-                                <li><a href="faq.html" class=" hvr-bounce-to-right"><i
-                                                class="fa fa-question-circle nav_icon"></i>FAQ</a></li>
-                                <li><a href="blank.html" class=" hvr-bounce-to-right"><i
-                                                class="fa fa-file-o nav_icon"></i>Blank</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="layout.html" class=" hvr-bounce-to-right"><i class="fa fa-th nav_icon"></i> <span
-                                        class="nav-label">Grid Layouts</span> </a>
-                        </li>
-
-                        <li>
-                            <a href="#" class=" hvr-bounce-to-right"><i class="fa fa-list nav_icon"></i> <span
-                                        class="nav-label">Forms</span><span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li><a href="forms.html" class=" hvr-bounce-to-right"><i
-                                                class="fa fa-align-left nav_icon"></i>Basic forms</a></li>
-                                <li><a href="validation.html" class=" hvr-bounce-to-right"><i
-                                                class="fa fa-check-square-o nav_icon"></i>Validation</a></li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <a href="#" class=" hvr-bounce-to-right"><i class="fa fa-cog nav_icon"></i> <span
-                                        class="nav-label">Settings</span><span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li><a href="signin.html" class=" hvr-bounce-to-right"><i
-                                                class="fa fa-sign-in nav_icon"></i>Signin</a></li>
-                                <li><a href="signup.html" class=" hvr-bounce-to-right"><i
-                                                class="fa fa-sign-in nav_icon"></i>Singup</a></li>
-                            </ul>
-                        </li>
                     </ul>
                 </div>
             </div>
@@ -240,11 +179,11 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
         </div>
     </div>
 </div>
-    <!---->
-    <!--scrolling js-->
-    <script src="/js/jquery.nicescroll.js"></script>
-    <script src="/js/scripts.js"></script>
-    <!--//scrolling js-->
-    <script src="/js/bootstrap.min.js"></script>
+<!---->
+<!--scrolling js-->
+<script src="/js/jquery.nicescroll.js"></script>
+<script src="/js/scripts.js"></script>
+<!--//scrolling js-->
+<script src="/js/bootstrap.min.js"></script>
 </body>
 </html>
